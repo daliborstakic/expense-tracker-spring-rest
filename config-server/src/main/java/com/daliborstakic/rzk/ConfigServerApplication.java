dc8690b4dc1dff1ac0e92e1baf0b6edfb7cd9b6f
daliborstakic <stakicdalibor64@gmail.com>
2024-01-15 15:16:41 +0100

    refactor: Add annotations and formatting

--- config-server/src/main/java/com/daliborstakic/rzk/ConfigServerApplication.java
+++ config-server/src/main/java/com/daliborstakic/rzk/ConfigServerApplication.java
@@ -2,12 +2,12 @@ package com.daliborstakic.rzk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-
 }
