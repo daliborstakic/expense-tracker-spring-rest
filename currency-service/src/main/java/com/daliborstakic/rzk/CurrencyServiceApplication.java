dda8375c816f51402bab2cfcc19594afa28d74c3
daliborstakic <stakicdalibor64@gmail.com>
2024-01-15 18:01:37 +0100

    refactor: Change minor config information

--- currency-service/src/main/java/com/daliborstakic/rzk/CurrencyServiceApplication.java
+++ currency-service/src/main/java/com/daliborstakic/rzk/CurrencyServiceApplication.java
@@ -2,12 +2,12 @@ package com.daliborstakic.rzk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("com.daliborstakic.rzk.model")
 @SpringBootApplication
 public class CurrencyServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyServiceApplication.class, args);
 	}
-
 }
